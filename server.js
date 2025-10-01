2ac3ec3d1f0584c10e2e490d14e0458670b5dc1a
kimjava911 <aibackend4th@gmail.com>
2025-10-01 11:16:02 +0900

    push

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-// es-module -> import, commonjs -> require
-const express = require("express"); // express 안에 있는 이미 구현되어 있는 코드들을 express 객체 형태로 불러오겠다
-const cors = require("cors"); // 설치한 의존성 패키지 cors를 불러오기
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { createClient } = require("@supabase/supabase-js"); // 구조분해 할당
 
-const app = express(); // () -> 호출해서 사용하겠다
-// 포트 -> 컴퓨터 서비스가 1개만 있는게 아님. email, db, server 1, server 2...
-// 1 ~ 2xxxx. => 이 번호로 오세요...
-const port = 3000; // cra. next -> express. / 5173.
-// localhost -> 3000. / 5500? <-> 구분해주는 의미.
+dotenv.config(); // .env -> KEY => SUPABASE_KEY
+// NODE -> process.env (환경변수) // cf. env file
 
-// CORS 해결을 위한 미들웨어 적용
-app.use(cors()); // 모든 출처에 대한 허용 (보안적으로 바람직하지 X)
+// const supabaseKey = process.env.SUPABASE_KEY;
+// const supabaseUrl = process.env.SUPABASE_URL;
+const { SUPABASE_KEY: supabaseKey, SUPABASE_URL: supabaseUrl } = process.env;
+console.log("supabaseKey", supabaseKey); // 확인 (npm run dev)
+console.log("supabaseUrl", supabaseUrl);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-// get, post...
-// app.방식(접속경로, 핸들러)
-// localhost:3000/
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
 app.get("/", (req, res) => {
-  // req -> request -> 전달 받은 데이터나 요청사항
-  // res -> response -> 응답할 내용/방식을 담은 객체
-  //   res.send("hello");
   res.send("bye");
 });
 
-// DOM listener / server '대기' -> 특정한 요청. -> 응답.
 app.listen(port, () => {
   console.log(`서버가 ${port}번 포트로 실행 중입니다.`);
 });
